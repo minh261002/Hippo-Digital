11357428f7ddcfb91d9c49880a9096b7dd8006d6
Cong Minh Tran <117755661+minh261002@users.noreply.github.com>
2024-07-30 15:38:06 +0700

    Update webhooks.ts

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -99,7 +99,7 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: 'DigitalHippo <hello@joshtriedcoding.com>',
+        from: 'HippoDigital <trcongminh.261002@gmail.com>',
         to: [user.email],
         subject:
           'Thanks for your order! This is your receipt.',
