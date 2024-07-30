b131329ecafd2b1578b764427291641b67922f2a
Cong Minh Tran <117755661+minh261002@users.noreply.github.com>
2024-07-30 14:30:24 +0700

    Update get-payload.ts

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,8 +46,8 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'hello@joshtriedcoding.com',
-        fromName: 'DigitalHippo',
+        fromAddress: 'trcongminh.261002@gmail.com',
+        fromName: 'HippoDigital',
       },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
