d0e5896f7a098e233a3b56ca62d71e2158b78c64
Cong Minh Tran <117755661+minh261002@users.noreply.github.com>
2024-07-30 14:31:35 +0700

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,13 +22,13 @@ export function formatPrice(
     style: 'currency',
     currency,
     notation,
-    maximumFractionDigits: 2,
+    maximumFractionDigits: 0,
   }).format(numericPrice)
 }
 
 export function constructMetadata({
-  title = 'DigitalHippo - the marketplace for digital assets',
-  description = 'DigitalHippo is an open-source marketplace for high-quality digital goods.',
+  title = 'HippoDigital - the marketplace for digital assets',
+  description = 'HippoDigital is an open-source marketplace for high-quality digital goods.',
   image = '/thumbnail.png',
   icons = '/favicon.ico',
   noIndex = false,
@@ -59,7 +59,7 @@ export function constructMetadata({
       creator: '@joshtriedcoding',
     },
     icons,
-    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    metadataBase: new URL('https://hippo-digital.onrender.com'),
     ...(noIndex && {
       robots: {
         index: false,
